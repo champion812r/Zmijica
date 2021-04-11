07d511a234330646980a4f9e1ce1dfadcc1032b5
Dimitrije Djordjevic <dimitrije1208@gmail.com>
2021-04-11 17:31:51 +0200

    dodao sve radi provere da li radi kod timmejta

---
+++ Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Zmijica
+{
+    class Snake
+    {
+        List<Point> teloZmije = new List<Point>();
+        public Snake()
+        {
+
+        }
+    }
+}
