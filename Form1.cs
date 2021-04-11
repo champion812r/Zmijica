ab2d4ab372e5b0261cc8b6a6cd1674085dc367ef
Dimitrije Djordjevic <dimitrije1208@gmail.com>
2021-04-11 17:28:18 +0200

    prvo dodavanje Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,33 @@ namespace Zmijica
 {
     public partial class Form1 : Form
     {
+        Snake zmija;
         public Form1()
         {
             InitializeComponent();
+            zmija = new Snake();
+
+        }
+        public void DrawRectangleFloat(PaintEventArgs e)
+        {
+            // Create pen.
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            // Create location and size of rectangle.
+            float x = 0.0F;
+            float y = 0.0F;
+            float width = 200.0F;
+            float height = 200.0F;
+
+            // Draw rectangle to screen.
+            e.Graphics.DrawRectangle(blackPen, x, y, width, height);
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            Graphics grafika = this.CreateGraphics();
+
+        }
     }
 }
