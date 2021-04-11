6d6123f935b9a1be91bc203bd8dee2d66c1174fd
Dimitrije Djordjevic <dimitrije1208@gmail.com>
2021-04-11 20:49:53 +0200

    proba gameGrid-a

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Zmijica
 {
-    partial class Form1
+    partial class SnakeGraphics
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,32 +29,58 @@ namespace Zmijica
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.gameGrid = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // gameGrid
             // 
-            this.panel1.Location = new System.Drawing.Point(171, 54);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(446, 302);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.gameGrid.Location = new System.Drawing.Point(12, 12);
+            this.gameGrid.Name = "gameGrid";
+            this.gameGrid.Size = new System.Drawing.Size(631, 421);
+            this.gameGrid.TabIndex = 0;
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(652, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Prikazi full grid";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(652, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ocisti grid";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SnakeGraphics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gameGrid);
+            this.Name = "SnakeGraphics";
+            this.Text = "Snake Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.SnakeGraphics_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel gameGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,35 +10,77 @@ using System.Windows.Forms;
 
 namespace Zmijica
 {
-    public partial class Form1 : Form
+    public partial class SnakeGraphics : Form
     {
-        Snake zmija;
-        public Form1()
+        Snake zmija=new Snake();
+        SolidBrush snakeBodyBrush = new SolidBrush(Color.Red);
+        Pen snakeBodyBorderPen = new Pen(Color.Black);
+        Graphics g,f;
+
+        int snakeBodySize=30;
+        int gridWidth, gridHeight, gridX, gridY;
+
+        private void SnakeGraphics_Paint(object sender, PaintEventArgs e)
+        {
+            //g.FillRectangle(snakeBodyBrush, new Rectangle(0, 0, 30, 30));
+            //g.DrawRectangle(snakeBodyBorderPen, new Rectangle(gridX-1,gridY-1,gridWidth+1,gridHeight+1));
+            g.DrawRectangle(snakeBodyBorderPen, new Rectangle(0,0,gridWidth-1,gridHeight-1));
+
+        }
+
+        public SnakeGraphics()
         {
             InitializeComponent();
-            zmija = new Snake();
+            f = this.CreateGraphics();
+            g = gameGrid.CreateGraphics();
 
-        }
-        public void DrawRectangleFloat(PaintEventArgs e)
-        {
-            // Create pen.
-            Pen blackPen = new Pen(Color.Black, 3);
+            gridX = gameGrid.Location.X;
+            gridY = gameGrid.Location.Y;
+            gridWidth = gameGrid.Width;
+            gridHeight = gameGrid.Height;
 
-            // Create location and size of rectangle.
-            float x = 0.0F;
-            float y = 0.0F;
-            float width = 200.0F;
-            float height = 200.0F;
+            //string msg = String.Format("{0} {1} {2} {3}", gridX - 1, gridY - 1, gridWidth + 1, gridHeight + 1);
+            //MessageBox.Show(msg);
 
-            // Draw rectangle to screen.
-            e.Graphics.DrawRectangle(blackPen, x, y, width, height);
+            //g = gameGrid.CreateGraphics();
+
+            
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            SolidBrush myBrush = new SolidBrush(Color.Red);
-            Graphics grafika = this.CreateGraphics();
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Rectangle rec = new Rectangle(0, 0, 50, 50);
+            //g.FillRectangle(snakeBodyBrush, new Rectangle(0, 0, 300, 300));
+            //g.DrawRectangle(snakeBodyBorderPen,new Rectangle(0,0,300,300));
+            //snakeBodyBrush.Dispose(); g.Dispose();
+
+            for(int y=0; y<=gridHeight-snakeBodySize; y+=snakeBodySize)
+            {
+                for(int x=0; x<=gridWidth-snakeBodySize; x+=snakeBodySize)
+                {
+                    g.FillRectangle(snakeBodyBrush, new Rectangle(x, y, snakeBodySize, snakeBodySize));
+                    g.DrawRectangle(snakeBodyBorderPen, new Rectangle(x, y, snakeBodySize, snakeBodySize));
+                }
+            }
+
+            /*for (int y = 0; y <= gridHeight - snakeBodySize; y += snakeBodySize)
+            {
+                for (int x = 0; x <= gridWidth - snakeBodySize; x += snakeBodySize)
+                {
+                    Label p = new Label();
+                    p.Location = new Point(x, y);
+                    p.Size = new Size(snakeBodySize, snakeBodySize);
+                    p.BackColor = Color.Red;
+                    p.Enabled=false;
+                    gameGrid.Controls.Add(p);
+                }
+            }*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameGrid.Invalidate(new Rectangle(1,1, gridWidth - 2, gridHeight -2));
         }
     }
 }
