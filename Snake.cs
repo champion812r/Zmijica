43cac6c6d573e0e2582417295c957c1d918db1ff
kristinakarasicevic115 <kristinakarasicevic115@gmail.com>
2021-04-13 17:18:12 +0200

    class Snake, funkcija za update liste teloZmije i slanje instrukcija

--- Snake.cs
+++ Snake.cs
@@ -10,24 +10,46 @@ namespace Zmijica
     class Snake
     {
         List<Point> teloZmije = new List<Point>();
-        int visinaMatrice, sirinaMatrice;
+        int visinaMatrice, sirinaMatrice,x, y;
 
-        public Snake(int visinaMatrice, int sirinaMatrice)
+        public Snake(int visinaMatrice, int sirinaMatrice, int x, int y)
         {
             this.visinaMatrice = visinaMatrice;
             this.sirinaMatrice = sirinaMatrice;
+            this.x = x;
+            this.y = y;
+            for (int i = 0; i < 5; i++)
+            {
+                Point deo = new Point { X = x-i, Y = y };
+                teloZmije.Add(deo);
+            }
         }
 
 
-        /*private List<...> lista (int direction)
+        private List<Instrukcija> lista (int direction)
         {
-            ///
-            List<...> Lista = new Lista<..>();
-
-            ///
-            ///
+            List<Instrukcija> Lista = new List<Instrukcija>();
+   
 
             return Lista;
-        }*/
+        }
+
+        private void ZmijaUpdate(ref List<Point> teloZmije)
+        {
+            teloZmije.RemoveAt(teloZmije.Count - 1);
+            Point glava = new Point { X = 0, Y = 1 };
+            teloZmije.Insert(0, glava);
+            
+        }
+
+        struct Instrukcija
+        {
+            public int x, y;
+            public bool oboj;  // true = oboj, false = obrisi
+            public bool zmija;  // true = zmija, false = hrana
+        }
+
+
     }
+
 }
