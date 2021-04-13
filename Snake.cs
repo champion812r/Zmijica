81b121706213099c6e7abb11eea13c074d6e9c3d
kristinakarasicevic115 <kristinakarasicevic115@gmail.com>
2021-04-13 17:24:58 +0200

    class Snake, funkcija za update liste teloZmije i slanje instrukcija (novo)

--- Snake.cs
+++ Snake.cs
@@ -10,43 +10,104 @@ namespace Zmijica
     class Snake
     {
         List<Point> teloZmije = new List<Point>();
-        int visinaMatrice, sirinaMatrice,x, y;
-
+        int visinaMatrice, sirinaMatrice, x, y, velicina=3;
+        Point hrana = new Point { X = 5, Y = 3 }; //privremena hrana
         public Snake(int visinaMatrice, int sirinaMatrice, int x, int y)
         {
             this.visinaMatrice = visinaMatrice;
             this.sirinaMatrice = sirinaMatrice;
             this.x = x;
             this.y = y;
-            for (int i = 0; i < 5; i++)
-            {
-                Point deo = new Point { X = x-i, Y = y };
-                teloZmije.Add(deo);
-            }
+            NapraviNovuZmiju(velicina); //nisam sigurna da l ovo treba da stoji ovde
+
         }
+        
 
-
-        private List<Instrukcija> lista (int direction)
+        public List<Instrukcija> lista (int direction)
         {
             List<Instrukcija> Lista = new List<Instrukcija>();
-   
+            Point glava = new Point { X = teloZmije[0].X , Y = teloZmije[0].Y };
+            if(direction==1)
+            {
+                glava.Y--;
+                if (glava.Y < 0) glava.Y = visinaMatrice; //ako predje zid
+                if (glava==teloZmije[0]) glava.Y++;       //ako ide unazad
+                ZmijaUpdate(ref teloZmije, glava, ref Lista);  //menjanje pozicija u listi, dodavanje instrukcija
+            }
+            else if (direction == 2)
+            {
+                glava.Y++;
+                if (glava.Y > visinaMatrice) glava.Y = 0;
+                if (glava == teloZmije[0]) glava.Y--;
+                ZmijaUpdate(ref teloZmije, glava, ref Lista);
+            }
+            else if (direction == 3)
+            {
+                glava.X--;
+                if (glava.X < 0) glava.X = sirinaMatrice;
+                if (glava == teloZmije[0]) glava.X++;
+                ZmijaUpdate(ref teloZmije, glava, ref Lista);
+            }
+            else if (direction == 4)
+            {
+                glava.X++;
+                if (glava.Y > sirinaMatrice) glava.Y = 0;
+                if (glava == teloZmije[0]) glava.X--;
+                ZmijaUpdate(ref teloZmije, glava, ref Lista);
+            }
 
             return Lista;
         }
 
-        private void ZmijaUpdate(ref List<Point> teloZmije)
+        private void ZmijaUpdate(ref List<Point> teloZmije, Point glava, ref List<Instrukcija> Lista)
         {
-            teloZmije.RemoveAt(teloZmije.Count - 1);
-            Point glava = new Point { X = 0, Y = 1 };
-            teloZmije.Insert(0, glava);
-            
+            Instrukcija instrukcija = new Instrukcija();
+            if (teloZmije.Contains(glava)) //da li se sudara
+            {
+                glava.X = -1;
+                instrukcija.xy = glava;
+                Lista.Add(instrukcija);
+                NapraviNovuZmiju(velicina);
+            }
+            else
+            {
+                teloZmije.Insert(0, glava);
+                instrukcija.xy = glava;
+                instrukcija.oboj = true;
+                instrukcija.telo = true;
+                Lista.Add(instrukcija);
+
+                if (glava == hrana) //ako pokupi hranu
+                {
+                    //NapraviNovuHranu(ref instrukcija);
+                    //Lista.Add(instrukcija);
+                }
+                else 
+                {
+                    instrukcija.xy = teloZmije[teloZmije.Count - 1];
+                    instrukcija.oboj = false;
+                    instrukcija.telo = true;
+                    Lista.Add(instrukcija);
+                    teloZmije.RemoveAt(teloZmije.Count - 1);
+                }
+            }
         }
 
-        struct Instrukcija
+        private void NapraviNovuZmiju(int velicina)
         {
-            public int x, y;
+            teloZmije = new List<Point>();
+            for (int i = 0; i <= velicina; i++)
+            {
+                Point deo = new Point { X = x - i, Y = y };
+                teloZmije.Add(deo);
+            }
+        }
+
+        public struct Instrukcija
+        {
+            public Point xy;
             public bool oboj;  // true = oboj, false = obrisi
-            public bool zmija;  // true = zmija, false = hrana
+            public bool telo;  // true = zmija, false = hrana
         }
 
 
